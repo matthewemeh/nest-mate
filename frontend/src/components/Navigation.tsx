44b552a1c523f96017e1f45b47940d859fddbebd
matthewemeh <mcmekus10@gmail.com>
2024-07-12 08:20:01 +0100

    navigation tweaks

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,5 +1,5 @@
+import { ImEnter } from 'react-icons/im';
 import { BsGridFill } from 'react-icons/bs';
-import { FiUserCheck } from 'react-icons/fi';
 import { MdOutlineBallot } from 'react-icons/md';
 import { RiHome4Line, RiUser3Line, RiHotelLine } from 'react-icons/ri';
 
@@ -11,8 +11,8 @@ import { useAppSelector } from 'hooks/useRootStorage';
 
 const Navigation = () => {
   const isAuthorized: boolean = useAuth();
-  const { HOME, PROFILE, HOSTELS, RESERVATIONS, DASHBOARD, ENTRIES } = PATHS;
   const { prefersDarkMode } = useAppSelector(state => state.userData);
+  const { HOME, PROFILE, HOSTELS, RESERVATIONS, DASHBOARD, ENTRIES } = PATHS;
 
   return (
     <nav
@@ -20,14 +20,20 @@ const Navigation = () => {
         prefersDarkMode && 'dark:text-zircon dark:bg-nile-blue-900'
       }`}>
       <ul className={`text-oslo-gray flex flex-col gap-2`}>
-        <li>
-          <NavigationTab to={HOME} icon={<RiHome4Line className='text-current' />} />
-        </li>
+        {isAuthorized ? (
+          <li>
+            <NavigationTab
+              to={DASHBOARD}
+              icon={<BsGridFill className='text-current text-[21px]' />}
+            />
+          </li>
+        ) : (
+          <li>
+            <NavigationTab to={HOME} icon={<RiHome4Line className='text-current' />} />
+          </li>
+        )}
         <li>
           <NavigationTab to={HOSTELS} icon={<RiHotelLine className='text-current text-[21px]' />} />
-        </li>
-        <li>
-          <NavigationTab to={PROFILE} icon={<RiUser3Line className='text-current text-[21px]' />} />
         </li>
         {isAuthorized && (
           <>
@@ -38,19 +44,13 @@ const Navigation = () => {
               />
             </li>
             <li>
-              <NavigationTab
-                to={DASHBOARD}
-                icon={<BsGridFill className='text-current text-[21px]' />}
-              />
-            </li>
-            <li>
-              <NavigationTab
-                to={ENTRIES}
-                icon={<FiUserCheck className='text-current text-[21px]' />}
-              />
+              <NavigationTab to={ENTRIES} icon={<ImEnter className='text-current text-[21px]' />} />
             </li>
           </>
         )}
+        <li>
+          <NavigationTab to={PROFILE} icon={<RiUser3Line className='text-current text-[21px]' />} />
+        </li>
       </ul>
     </nav>
   );
